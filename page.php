0bccdb5ec10961f35ec4b048d150cc2fca6c408c
ShreyPathak <shreypathak53@gmail.com>
2023-02-13 23:16:31 +0530

    test website

---
+++ page.php
@@ -0,0 +1,31 @@
+<?php
+$servername = "localhost";
+$username = "id20299093_shrey";
+$password = "TigerPathak@2003";
+$dbname = "id20299093_shreypathak";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+if (isset($_POST['username']) && isset($_POST['email']) && isset($_POST['password'])) {
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $sql = "INSERT INTO users (username, email, password)
+    VALUES ('$username', '$email', '$password')";
+
+    if (mysqli_query($conn, $sql)) {
+        echo "New record created successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+}
+
+mysqli_close($conn);
+?>
